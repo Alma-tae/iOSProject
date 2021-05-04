37e8aeb53e96761b3cb01138862351a78e7a94db
Taehui Kim <taehuikim0430@gmail.com>
2021-05-04 13:19:26 +0900

    Core Data 2

---
+++ CoreDataToDo/LogVC.swift
@@ -0,0 +1,110 @@
+//
+//  LogVC.swift
+//  CoreDataToDo
+//
+//  Created by Sinchon on 2021/05/04.
+//
+
+import UIKit
+
+//데이터의 삽입, 수정, 삭제를 나타내기 위함 enum
+//enum은 데이터를 제한하고자 할 때 사용
+public enum LogType:Int16{
+    case create = 0
+    case edlt = 1
+    case delete = 2
+}
+
+//Int16 클래스에 기능을 추가
+//swift나 Kotlin, C#, javascript는 기존 클래스나 객체에 기능 추가 가능 => 별도의 클래스 생성할 필요 없음
+extension Int16{
+    func toLogType() -> String{
+        switch self {
+        case 0 : return "생성"
+        case 1 : return "수정"
+        case 2 : return "삭제"
+        default : return ""
+        }
+    }
+}
+
+class LogVC: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+    /*
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+    */
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- CoreDataToDo/ToDoInputVC.swift
+++ CoreDataToDo/ToDoInputVC.swift
@@ -6,8 +6,18 @@
 //
 
 import UIKit
+import CoreData
 
 class ToDoInputVC: UIViewController {
+    //삽입과 수정을 구분하기 위한 프로퍼티
+    var mode = "삽입"
+    
+    //수정을 위해 필요한 데이터
+    var object : NSManagedObject!
+    
+    var t = ""
+    var c = ""
+    var r = Date()
 
     @IBOutlet weak var dpRuntime: UIDatePicker!
     @IBOutlet weak var tvContent: UITextView!
@@ -21,17 +31,30 @@ class ToDoInputVC: UIViewController {
         
         //자신을 출력한 뷰 컨트롤러 찾아오기
         //NavigationController를 이용해서 이동한 경우
-        let navVC = self.presentingViewController as! UINavigationController
-        let toDoListVC = navVC.topViewController as! ToDoListVC
+        //첫번째 뷰 컨트롤러를 찾아오기
+        
+       // let navVC = self.presentationController as! UINavigationController
+       // let toDoListVC = navVC.topViewController as! ToDoListVC
+        
+        
+        let toDoListVC = navigationController!.viewControllers[0] as! ToDoListVC
         
         //데이터 삽입하는 메서드 호출
-        let result = toDoListVC.save(title: title!, content: content!, runtime: runtime)
-        if result == true{
-            NSLog("데이터 삽입 성공")
+        if mode == "삽입"{
+            let result = toDoListVC.save(title: title!, content: content!, runtime: runtime)
+            if result == true{
+                NSLog("데이터 삽입 성공")
+            }else{
+                NSLog("데이터 삽입 실패")
+            }
         }else{
-            NSLog("데이터 삽입 실패")
+            let result = toDoListVC.edit(object: object, title: title!, content: content!, runtime: runtime)
+            if result == true{
+                NSLog("데이터 삽입 성공")
+            }else{
+                NSLog("데이터 삽입 실패")
+            }
         }
-        
         //이전 뷰 컨트롤러로 이동
         self.navigationController?.popViewController(animated: true)
     }
@@ -44,7 +67,14 @@ class ToDoInputVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        self.title = mode
+        
+        //수정 모드이면 데이터를 출력
+        if mode == "수정"{
+            self.tfTitle.text = t
+            self.tvContent.text = c
+            self.dpRuntime.date = r
+        }
     }
     
 
